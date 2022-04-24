cac29cbbeca2bb109977821bfa9f51c377b9e983
Luke Bamtefa <41142438+heisluke@users.noreply.github.com>
2022-04-24 16:12:10 +0100

    created the navbar
    not complete yet, i'll have to create an alternate logo color

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import logo from './brand/logo.svg'
+import React from 'react'
 import './App.css'
+import Navbar from './navbar'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
+	<Router>
+		<Navbar/>
+		<header className="App-header">
+		<img src={logo} className="App-logo" alt="logo" />
+		This is KingsMeal
+		<p>A restaurant on the go!</p>
+		</header>	
+	</Router>
+		
+	
 	)
 }
 
---
+++ src/navbar/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import logo from '../brand/logo.svg'
+import { Nav, NavLink, NavBtn, NavMenu, NavBtnLink, Bars } from "./navbarElements";
+
+const Navbar = () => {
+    return ( 
+       <>
+       <Nav>
+           <NavLink to="/">
+               {/*<h1>Logo</h1>*/}
+               <img src={logo} alt="logo" />
+           </NavLink>
+           <Bars />
+
+
+           <NavMenu>
+               <NavLink to="/foodlist" activeStyle>
+                Our Menu
+               </NavLink>
+               <NavLink to="/about" activeStyle>
+                About Us
+               </NavLink>
+               <NavLink to="/contact" activeStyle>
+                Contact Us
+               </NavLink>
+               <NavLink to="/signin" activeStyle>
+                Sign In
+               </NavLink>
+           </NavMenu>
+
+           <NavBtn>
+               <NavBtnLink to="/signup">
+                Sign In
+               </NavBtnLink>
+           </NavBtn>
+       </Nav>
+       </>
+     );
+}
+ 
+export default Navbar;
