b1df0f0d8c58edb60a4081dd835c0739e3d4fb1c
Eva Raymond <evathecoderr@outlook.com>
2022-04-25 12:45:57 +0100

    restructue code base

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import logo from './brand/logo.svg'
-import React from 'react'
-import './App.css'
-import Navbar from './navbar'
 import { BrowserRouter as Router } from 'react-router-dom'
+import Navbar from './components/navbar'
+import logo from './brand/logo.svg'
+import './App.css'
 
 function App() {
 	return (
-	<Router>
-		<Navbar/>
-		<header className="App-header">
-		<img src={logo} className="App-logo" alt="logo" />
-		This is KingsMeal
-		<p>A restaurant on the go!</p>
-		</header>	
-	</Router>
-		
-	
+		<Router>
+			<Navbar />
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				This is KingsMeal
+				<p>A restaurant on the go!</p>
+			</header>
+		</Router>
 	)
 }
 
