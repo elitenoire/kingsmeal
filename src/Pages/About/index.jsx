b72507f27d8bce9afe42a0b18347393423bdeef2
Eva Raymond <evathecoderr@outlook.com>
2022-04-26 23:24:41 +0100

    refactor about page

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,71 +1,77 @@
-import React from "react";
-
 // importing images
-import multiple from "../assets/images/multiple.png";
-import person from "../assets/images/person.png";
+import multiple from "../../assets/images/multiple.jpg";
+import evaUrl from "../../assets/images/eva.webp";
+import elsaUrl from "../../assets/images/elsa.webp";
+import fatimaUrl from "../../assets/images/fatima.webp";
+import andreyUrl from "../../assets/images/andrey.webp";
+import lukeUrl from "../../assets/images/luke.webp";
+import demrysUrl from "../../assets/images/demrys.webp";
 
 // importing css file
-import "./css/About.css";
+import "./About.css";
 
 export default function About() {
   return (
     <>
-      <div className="imgBanner">
+      <div className="about-img-banner">
         <img src={multiple} alt="different food" />
       </div>
-
-      <main>
+      <div className="about-container">
         <h1>About Us</h1>
         <p>
-          KingsMeal is a restaurant on the go! Here we have a variety of
+          <span className="highlight">KingsMeal</span> is a restaurant on the go! Here we have a variety of
           different cuisines and dishes to choose from.
         </p>
 
         <h2>Name</h2>
         <p>
-          <span>KingsMeal</span> the name comes from the pride we have in our
+          <span className="highlight">KingsMeal</span> the name comes from the pride we have in our
           different and rich cuisines and the service we provide to our
           customers. We provide the king level services with top hygiene.
         </p>
 
         <h2>Founders</h2>
         <p>
-          <span>KingsMeal</span> was founded by six friends who worked together
+          <span className="highlight">KingsMeal</span> was founded by six friends who worked together
           to make this a reality.
         </p>
 
-        <ul className="grid">
+        <ul className="about-grid">
           <li>
-            <img src={person} alt="person" />
+            <img src={evaUrl} alt="person" />
             <a href="https://github.com/elitenoire">Eva Raymond</a>
           </li>
           <li>
-            <img src={person} alt="person" />
+            <img src={fatimaUrl} alt="person" />
             <a href="https://github.com/tab21">Tabassum Fatima</a>
           </li>
           <li>
-            <img src={person} alt="person" />
+            <img src={demrysUrl} alt="person" />
             <a href="https://github.com/d-emrys">d-emrys</a>
           </li>
           <li>
-            <img src={person} alt="person" />
+            <img src={lukeUrl} alt="person" />
             <a href="https://github.com/heisluke">Luke Bamtefa</a>
           </li>
           <li>
-            <img src={person} alt="person" />
+            <img src={andreyUrl} alt="person" />
             <a href="https://github.com/andreyscott">Andrew Irorere</a>
           </li>
           <li>
-            <img src={person} alt="person" />
+            <img src={elsaUrl} alt="person" />
             <a href="https://github.com/elsashai">Elsa</a>
           </li>
         </ul>
+
+        <p className="about-shoutout">We are a team of young & beautiful people with a passion for tasty and traditional food.
+          <span>- Team KingsMeal</span>
+        </p>
 
         <div className="flex">
           <div className="flexItem">
             <h2>Business Hours</h2>
             <div className="flex">
-              <div className="left">
+              <div className="about-align-left">
                 <p>
                   Tuesday-Friday :
                   <br />
@@ -75,7 +81,7 @@ export default function About() {
                 </p>
               </div>
 
-              <div className="right">
+              <div className="about-align-right">
                 <p>
                   11:00am-9:00pm
                   <br /> 11:00am-11:00pm
@@ -83,20 +89,19 @@ export default function About() {
                 </p>
               </div>
             </div>
-            
-            <p>INDOOR & OUTDOOR SEATING, TAKEOUT, AND DELIVERY AVAILABLE</p>
+            <p className="about-info">INDOOR & OUTDOOR SEATING, TAKEOUT, AND DELIVERY AVAILABLE</p>
           </div>
 
           <div className="flexItem">
             <h2>Location</h2>
             <p>
-              <span>KingsMeal</span> is located at
+              <span className="highlight">KingsMeal</span> is located at
               <br />
               Virtual address: Near the corner of the Eiffel Tower street.
             </p>
           </div>
         </div>
-      </main>
+      </div>
     </>
   );
 }
