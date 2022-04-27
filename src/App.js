91f93a899c6adfd362d6b974503c4edf41689e5d
andreyscott <andreyscott301@gmail.com>
2022-04-27 08:34:53 +0100

    staging the changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router } from 'react-router-dom'
-import Navbar from './components/navbar'
-import logo from './brand/logo.svg'
+// import Navbar from './components/navbar'
+// import logo from './brand/logo.svg'
 import './App.css'
+import Contactus from './contactus'
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 function App() {
 	return (
 		<Router>
-			<Navbar />
+			{/* <Navbar /> */}
+			<Routes>
+				<Route path='/' element={<Contactus />} />
+			</Routes>
+			<contactus />
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
+				{/* <img src={logo} className="App-logo" alt="logo" /> */}
 				This is KingsMeal
 				<p>A restaurant on the go!</p>
 			</header>
